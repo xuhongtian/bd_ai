455b5e2675f8f2fd86c8d827803258eb01d30bd1
xue <xue.wang@goupwith.com>
2021-07-02 11:47:34 +0800

    第二次提交－作业灰度图－０１

--- huidu-img.py
+++ huidu-img.py
@@ -7,10 +7,6 @@ if __name__ == "__main__":
     img = cv2.imread(img_path)
     #获取图片的宽和高
     width,height = img.shape[:2][::-1]
-    #将图片缩小便于显示观看
-
-    # img_resize = cv2.resize(img,(int(width*0.5),int(height*0.5)),interpolation=cv2.INTER_CUBIC),cv2.imshow("img",img)
-    # print("img_reisze shape:{}".format(np.shape(img_resize)))
 
     #将图片转为灰度图
     img_gray = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
